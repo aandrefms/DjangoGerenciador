ea955b81e5f7d761f6ca1294a5ce773a60c36f31
andre <andre.felippems@gmail.com>
2021-01-23 15:40:46 -0300

    23/01

--- paginas/models.py
+++ paginas/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 # Create your models here.
 class Insumo(models.Model):
-    nome = models.TextField(max_length=120)
+    nome = models.TextField(max_length=110)
     categoria = models.TextField()
     quantidade = models.IntegerField()
